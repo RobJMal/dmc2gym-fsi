6f79afe9be35adea8fd0fd9ade881ffd042fef15
Robert Jomar Malate <robjmal@gmail.com>
2024-05-18 17:10:45 +0200

    Created main.py file to test

---
+++ main.py
@@ -0,0 +1,11 @@
+import dmc2gym
+
+env = dmc2gym.make(domain_name='point_mass', task_name='easy', seed=1)
+
+done = False
+obs = env.reset()
+while not done:
+  action = env.action_space.sample()
+  obs, reward, done, info = env.step(action)
+  print("Observation: ", obs)
+  print("Reward: ", reward)
